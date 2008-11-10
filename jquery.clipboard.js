ef63d52842adeed6e95b0bc5006848becf9361bb
chrispatterson <dimmerswitch@gmail.com>
2008-11-10 09:58:31 -0600

    Add a variety of customizations to clickToClipboard, including the ability to append / prepend the clipboard text to a variety of targets. Also, function should now properly handle multiple clickToClipboard invocations on the same page

--- jquery.clipboard.js
+++ jquery.clipboard.js
@@ -2,49 +2,78 @@
  * Clipboard
  * A jQuery plugin for implementing "click to clipboard" functionality
  * 
- * Version 0.1
+ * Version 0.5
  * Author: Chris Patterson
  *
  * License: GPL 3 http://www.gnu.org/licenses/gpl-3.0.html
  * 
  * Inspired by:
  * ClipboardCopy (http://www.jeffothy.com/weblog/clipboard-copy/)
+ * Skitch (http://skitch.com)
  * 
+ * NOTE: this plugin depends upon jquery.flash.js
+ *
+ * ASSUMPTION: All elements which will have "click to clipboard" associated will need an id attribute
+ * Additionally, all elements which will be the target for insertion will need an id attribute
+ *
+ * [TODO]
+ * - consolidate current duped insert / timeout functions for the hasFlash / noFlash conditionals
+ * - allow customization of the element wrapping the clipboardText
+ * - (Possible) look at adding better noFlash clipboard support. Currently, we only support IE,
+ * as Opera / Mozilla were behaving wonkily in early testing of createTextRange support.
  **/
 (function($){
-  $.fn.clickToClipboard = function(options) {
-    
-  var defaults = {
-   fadeoutLength: 600,
-   fadeoutTimer: 1000,
-   clipboardText: "Copied to Clipboard"
-  };
-  
-  var options = $.extend(defaults, options);
-    
-	  return this.each(function() {
-	    var hasFlash = $(document).flash.hasFlash(7);
+	$.fn.clickToClipboard = function(options) {
+		
+	var defaults = {
+		fadeoutLength: 600,
+		fadeoutTimer: 1000,
+		clipboardText: 'Copied to Clipboard',
+		clipboardTextTarget: 'field', /* Allowed values: "field", "label", "form" */
+		clipboardTextInsert: 'after' /* Allowed values: "before" "after" */
+	};
+	
+	var options = $.extend(defaults, options);
+	var target = [];
+		
+		return this.each(function(intIndex) {
+	
+			var hasFlash = $(document).flash.hasFlash(7);
+
+			if (options.clipboardTextTarget == "form") {
+				target[intIndex] = $("form:has(#" + this.id +")");
+			} else if (options.clipboardTextTarget == "label") {
+				target[intIndex] = $("label[for=" + this.id +"]");
+			} else {
+				target[intIndex] = $(this);
+			}
 			obj = $(this);
-	    $('<div class="copied-to-clipboard" style="opacity: 0.999999; display: none;">' + options.clipboardText + '</div>').insertAfter(obj);
+
+			var insertText = '<span class="copied-to-clipboard" style="opacity: 0.999999; display: none;">' + options.clipboardText + '</span>';
+			(options.clipboardTextInsert == "before") ? $(insertText).insertBefore(target[intIndex]) : $(insertText).insertAfter(target[intIndex]);
 			if (hasFlash) {
-			  obj.click(function(event) {
+				obj.click(function(event) {
 					$('.flashcopier').flash({ src: '_clipboard.swf', width: 0, height: 0, flashvars: {clipboard: obj.value} }, { update: false });
-		      obj.next('.copied-to-clipboard').show();
-		      setTimeout("obj.next('.copied-to-clipboard').fadeOut(options.fadeoutLength)", options.fadeoutTimer);
-		      obj.blur();
+
+					(options.clipboardTextInsert == "before") ? target[intIndex].prev('.copied-to-clipboard:first').show() :	target[intIndex].next('.copied-to-clipboard:first').show();		
+					
+					(options.clipboardTextInsert == "before") ? setTimeout("$('#" + target[intIndex].attr("id")+"').prev('.copied-to-clipboard:first').fadeOut(" + options.fadeoutLength + ")", options.fadeoutTimer) : setTimeout("$('#" + target[intIndex].attr("id")+"').next('.copied-to-clipboard:first').fadeOut(" + options.fadeoutLength + ")", options.fadeoutTimer);
+					
+					obj.blur();
 				});
 				$('<div class="flashcopier">').appendTo('body');
 			} else if (window.clipboardData && sourceElement.createTextRange()) {
-			  obj.focus(function(event) {
-				  window.clipboardData.setData("Text",sourceElement.value);
-		      obj.next('.copied-to-clipboard').show();
-		      setTimeout("obj.next('.copied-to-clipboard').fadeOut(options.fadeoutLength);", options.fadeoutTimer);
-		      obj.blur();
-				});  
+				obj.focus(function(event) {
+					window.clipboardData.setData("Text",sourceElement.value);
+
+					(options.clipboardTextInsert == "before") ? target[intIndex].prev('.copied-to-clipboard:first').show() :	target[intIndex].next('.copied-to-clipboard:first').show();		
+					
+					(options.clipboardTextInsert == "before") ? setTimeout("$('#" + target[intIndex].attr("id")+"').prev('.copied-to-clipboard:first').fadeOut(" + options.fadeoutLength + ")", options.fadeoutTimer) : setTimeout("$('#" + target[intIndex].attr("id")+"').next('.copied-to-clipboard:first').fadeOut(" + options.fadeoutLength + ")", options.fadeoutTimer);
+					
+					obj.blur();
+				});	 
 			}
-   		this.blur(function(event){
-		    setTimeout("this.next('.copied-to-clipboard').fadeOut(options.fadeoutLength);", options.fadeoutTimer);
-		  });
-	  });
-  };
+		});
+	};
+
 })(jQuery);
